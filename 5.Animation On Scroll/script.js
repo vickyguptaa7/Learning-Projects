d02c831b4ffb2906b3a29fac9e3136fca6f13996
Vicky Gupta <vickyguptaa7@gmail.com>
2022-05-01 21:33:17 +0530

    proj on aos

---
+++ 5.Animation On Scroll/script.js
@@ -0,0 +1,9 @@
+AOS.init(
+    {
+        delay: 200, // values from 0 to 3000, with step 50ms
+        duration: 600, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+    }
+);
